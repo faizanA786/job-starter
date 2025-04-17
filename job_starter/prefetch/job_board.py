e2fead8f1b67e5a4752107890525d2c6015b43b7
Faizan Ahmed <119706887+faizanA786@users.noreply.github.com>
2025-04-17 21:43:07 +0100

    fix(job_board.py): Added try-catch

--- job_starter/prefetch/job_board.py
+++ job_starter/prefetch/job_board.py
@@ -19,29 +19,32 @@ class JobBoard:
         for i in range(0, len(roles)):
             print("checking " + roles[i] + " role")
 
-            response = requests.get("https://api.adzuna.com/v1/api/jobs/gb/search/" + str(page) + "?app_id=" + API_ID + "&app_key=" + API_KEY + "&results_per_page=10&what_exclude=" + exclude + "&title_only=" + roles[i] + "&where=East%20London&distance=10&max_days_old=3&sort_by=date&salary_include_unknown=1")
-
-            for job in response.json()["results"]:
-                if job["id"] not in jobs_seen:
-                    new_job = {}
-
-                    day_added = (job["created"].split("T")[0]).split("-")[-1] #only get the day of date
-                    if int(day_added) == day:
-                        new_job["date"] = "Today"
-                    elif int(day_added) == day-1:
-                        new_job["date"] = "Yesterday"
-                    elif int(day_added) == day-2:
-                        new_job["date"] = "2 days ago"
-                    elif int(day_added) == day-3:
-                        new_job["date"] = "3 days ago"
-
-                    new_job["contract_time"] = job.get("contract_time")  # might be unknown
-                    new_job["title"] = job["title"]
-                    new_job["company"] = job["company"]["display_name"]
-                    new_job["location"] = job["location"]["display_name"]
-                    new_job["url"] = job["redirect_url"]
-
-                    jobs.append(new_job)
-                    jobs_seen.add(job["id"])
-        print("end of page " + str(page))
+            try:
+                response = requests.get("https://api.adzuna.com/v1/api/jobs/gb/search/" + str(page) + "?app_id=" + API_ID + "&app_key=" + API_KEY + "&results_per_page=10&what_exclude=" + exclude + "&title_only=" + roles[i] + "&where=East%20London&distance=10&max_days_old=3&sort_by=date&salary_include_unknown=1")
+                for job in response.json()["results"]:
+                    if job["id"] not in jobs_seen:
+                        new_job = {}
+    
+                        day_added = (job["created"].split("T")[0]).split("-")[-1] #only get the day of date
+                        if int(day_added) == day:
+                            new_job["date"] = "Today"
+                        elif int(day_added) == day-1:
+                            new_job["date"] = "Yesterday"
+                        elif int(day_added) == day-2:
+                            new_job["date"] = "2 days ago"
+                        elif int(day_added) == day-3:
+                            new_job["date"] = "3 days ago"
+    
+                        new_job["contract_time"] = job.get("contract_time")  # might be unknown
+                        new_job["title"] = job["title"]
+                        new_job["company"] = job["company"]["display_name"]
+                        new_job["location"] = job["location"]["display_name"]
+                        new_job["url"] = job["redirect_url"]
+    
+                        jobs.append(new_job)
+                        jobs_seen.add(job["id"])
+                print("end of page " + str(page))
+            except Exception as error:
+                print("error, no jobs?")
+                continue
         return jobs
